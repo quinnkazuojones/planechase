cf71a508c4e8dc82bf7bd39fcb2b964df36ed87a
Quinn Jones <quinnjones0928@icloud.com>
2019-12-31 19:17:09 -0800

    Updates

---
+++ precache-manifest.53e253fdb368e4da85c82be1b120d432.js
@@ -0,0 +1,382 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4ca871daeb57fee22ba21f753899719",
+    "url": "/planechase/index.html"
+  },
+  {
+    "revision": "d894e6359879c29c2615",
+    "url": "/planechase/static/css/main.f40a4869.chunk.css"
+  },
+  {
+    "revision": "00add457b731f56dd732",
+    "url": "/planechase/static/js/2.74793c05.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/planechase/static/js/2.74793c05.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d894e6359879c29c2615",
+    "url": "/planechase/static/js/main.53be97b5.chunk.js"
+  },
+  {
+    "revision": "7447fa277ee1416cee27",
+    "url": "/planechase/static/js/runtime-main.6458f906.js"
+  },
+  {
+    "revision": "13b1331103d017734798722af22b4558",
+    "url": "/planechase/static/media/academy_at_toleria_west.13b13311.jpg"
+  },
+  {
+    "revision": "2867b3158455e03764a7c834a2d6f89a",
+    "url": "/planechase/static/media/agyrem.2867b315.jpg"
+  },
+  {
+    "revision": "8317fe11f140761a2d4df93ee8bcd6a0",
+    "url": "/planechase/static/media/akoum.8317fe11.jpg"
+  },
+  {
+    "revision": "990b374ef7f96a2601ab63872e0e63e1",
+    "url": "/planechase/static/media/aretopolis.990b374e.jpg"
+  },
+  {
+    "revision": "4d99e04ae78c1d1673a1b8fac998fd20",
+    "url": "/planechase/static/media/astral_arena.4d99e04a.jpg"
+  },
+  {
+    "revision": "2a53f6cf86d9f36325c9cbb3da9560a6",
+    "url": "/planechase/static/media/back.2a53f6cf.png"
+  },
+  {
+    "revision": "befb7051513e5ed64536aa208cb645f2",
+    "url": "/planechase/static/media/back.befb7051.svg"
+  },
+  {
+    "revision": "f88710cec309e1f1f479c540adc11878",
+    "url": "/planechase/static/media/bant.f88710ce.jpg"
+  },
+  {
+    "revision": "5f47a7a8a99f77a3fc7b93a6f56ace3d",
+    "url": "/planechase/static/media/chaos.5f47a7a8.svg"
+  },
+  {
+    "revision": "00a2f4c4352f4d710864a283a1a4a37a",
+    "url": "/planechase/static/media/chaotic_ether.00a2f4c4.jpg"
+  },
+  {
+    "revision": "06801076bbe8ebd2fa1981c5940aee1a",
+    "url": "/planechase/static/media/cliffside_market.06801076.jpg"
+  },
+  {
+    "revision": "1269982ff99e9d1f432ccca293f91380",
+    "url": "/planechase/static/media/dice.1269982f.svg"
+  },
+  {
+    "revision": "5f4c4a607619b958d682120953b014d7",
+    "url": "/planechase/static/media/edge_of_malacol.5f4c4a60.jpg"
+  },
+  {
+    "revision": "1705261e708bbda220212574ae1ff5de",
+    "url": "/planechase/static/media/eloren_wilds.1705261e.jpg"
+  },
+  {
+    "revision": "0a4a7b4e20ad75d6ae67140b76d06bc7",
+    "url": "/planechase/static/media/feeding_grounds.0a4a7b4e.jpg"
+  },
+  {
+    "revision": "3b3e306644c8c64f40db04e6f3a763c4",
+    "url": "/planechase/static/media/fields_of_summer.3b3e3066.jpg"
+  },
+  {
+    "revision": "fdc9cb7ae68fd18abe7b11ec376030a2",
+    "url": "/planechase/static/media/furnace_layer.fdc9cb7a.png"
+  },
+  {
+    "revision": "e4a9c0639f5705a3d5048b0314d90c9c",
+    "url": "/planechase/static/media/gavony.e4a9c063.jpg"
+  },
+  {
+    "revision": "b8e2d0761673173d3ad56fad7e387171",
+    "url": "/planechase/static/media/glen_elendra.b8e2d076.jpg"
+  },
+  {
+    "revision": "3c32bda76d7c073da4ed146c5f2ede2d",
+    "url": "/planechase/static/media/glimmervoid_basin.3c32bda7.jpg"
+  },
+  {
+    "revision": "f7bfe975693b403ea47b7e6099b76785",
+    "url": "/planechase/static/media/goldmeadow.f7bfe975.jpg"
+  },
+  {
+    "revision": "76a48f23f99d16b05af253d83b5ae1c6",
+    "url": "/planechase/static/media/grand_ossuary.76a48f23.png"
+  },
+  {
+    "revision": "e83abab87e294e15ea0cf875c0c84557",
+    "url": "/planechase/static/media/grixis.e83abab8.jpg"
+  },
+  {
+    "revision": "606cbf30e3da9c7bd935519b910eee10",
+    "url": "/planechase/static/media/grove_of_the_dreampods.606cbf30.jpg"
+  },
+  {
+    "revision": "1dabd14cd9752773b911e76ad37348fb",
+    "url": "/planechase/static/media/hedron_fields_of_agadeem.1dabd14c.png"
+  },
+  {
+    "revision": "9fb99f99ae047a416cdf8eebee225e61",
+    "url": "/planechase/static/media/immersturm.9fb99f99.jpg"
+  },
+  {
+    "revision": "d734a12d2ab5bd122560e785aed00d00",
+    "url": "/planechase/static/media/interplanar_tunnel.d734a12d.jpg"
+  },
+  {
+    "revision": "25116b6fd797f98776afa92fb18f9ce3",
+    "url": "/planechase/static/media/isle_of_vesuva.25116b6f.jpg"
+  },
+  {
+    "revision": "19e1c5d5251083396054b57782489ea2",
+    "url": "/planechase/static/media/izzet_steam_maze.19e1c5d5.jpg"
+  },
+  {
+    "revision": "c109301d3eaee94db1e4060cc751230c",
+    "url": "/planechase/static/media/jund.c109301d.jpg"
+  },
+  {
+    "revision": "7669b75853af827dc764016038dcd27f",
+    "url": "/planechase/static/media/kessig.7669b758.png"
+  },
+  {
+    "revision": "14deb2cb6d20eae4610e78ea02fbe028",
+    "url": "/planechase/static/media/kharasha_foothills.14deb2cb.png"
+  },
+  {
+    "revision": "ddfe67818ac500aa366c004b5631e78d",
+    "url": "/planechase/static/media/krosa.ddfe6781.jpg"
+  },
+  {
+    "revision": "cda57efb733c81c0ad3d6ba9aac7227f",
+    "url": "/planechase/static/media/lair_of_the_ashen_idol.cda57efb.jpg"
+  },
+  {
+    "revision": "295baded40d8e9a5f37c350383bb5e50",
+    "url": "/planechase/static/media/lethe_lake.295baded.jpg"
+  },
+  {
+    "revision": "5905de7f8419119a7ede1c403fc393f6",
+    "url": "/planechase/static/media/llanowar.5905de7f.jpg"
+  },
+  {
+    "revision": "f502c151e2b892b60ef9708e04313b57",
+    "url": "/planechase/static/media/logo.f502c151.svg"
+  },
+  {
+    "revision": "137e6ddb28d9ec600da29d2f80316496",
+    "url": "/planechase/static/media/minamo.137e6ddb.jpg"
+  },
+  {
+    "revision": "42cac7aa2a72670c75fb99dee457694a",
+    "url": "/planechase/static/media/morphic_tide.42cac7aa.jpg"
+  },
+  {
+    "revision": "d46fbdf33f600ceac9c76aeefd6b6879",
+    "url": "/planechase/static/media/mount_keralia.d46fbdf3.png"
+  },
+  {
+    "revision": "6098bc6585cd0f5ece63c748235efa78",
+    "url": "/planechase/static/media/murasa.6098bc65.jpg"
+  },
+  {
+    "revision": "2f0c9ec5cb086d561e3712e284c37736",
+    "url": "/planechase/static/media/mutual_epiphany.2f0c9ec5.jpg"
+  },
+  {
+    "revision": "2a1ade0751ef36cc297888fa981e8d3e",
+    "url": "/planechase/static/media/naar_isle.2a1ade07.jpg"
+  },
+  {
+    "revision": "74157fe4a5e535ee971aa02f0c6c2a18",
+    "url": "/planechase/static/media/naya.74157fe4.jpg"
+  },
+  {
+    "revision": "621e5c8b1b72e7e7d442d3b8317c3ccd",
+    "url": "/planechase/static/media/nephalia.621e5c8b.jpg"
+  },
+  {
+    "revision": "213b4545b402761bdf20bceec2113c6a",
+    "url": "/planechase/static/media/norns_dominion.213b4545.jpg"
+  },
+  {
+    "revision": "ba5a2b82ca3fe090440038a83d1c9d2e",
+    "url": "/planechase/static/media/null.ba5a2b82.svg"
+  },
+  {
+    "revision": "60416276663cc853b5019b614eb5de70",
+    "url": "/planechase/static/media/onakke_catacomb.60416276.jpg"
+  },
+  {
+    "revision": "917b59b400c4ab9d7a1ec2e4d3d9161d",
+    "url": "/planechase/static/media/orochi_colony.917b59b4.jpg"
+  },
+  {
+    "revision": "96ebb927087012d91ea9655aec66a4ab",
+    "url": "/planechase/static/media/orzhova.96ebb927.jpg"
+  },
+  {
+    "revision": "7323c9cc264ceb5ce42b175b205d9913",
+    "url": "/planechase/static/media/otaria.7323c9cc.jpg"
+  },
+  {
+    "revision": "eda06abf0d88295fa8c81b0c6151fd63",
+    "url": "/planechase/static/media/panopticon.eda06abf.jpg"
+  },
+  {
+    "revision": "0d3908066fb13c26981c0e194bc3c955",
+    "url": "/planechase/static/media/planewide_disaster.0d390806.jpg"
+  },
+  {
+    "revision": "42bbeb913eb49e45dc5e161d3016aef0",
+    "url": "/planechase/static/media/pools_of_becoming.42bbeb91.jpg"
+  },
+  {
+    "revision": "1e2d238cc0cdb1f2b9d31981a5614cf4",
+    "url": "/planechase/static/media/prahv.1e2d238c.jpg"
+  },
+  {
+    "revision": "cf01028fdb472d9b94fba971e4b48bc0",
+    "url": "/planechase/static/media/pw.cf01028f.svg"
+  },
+  {
+    "revision": "ed942dbd2ee62e7708f4c043e4528200",
+    "url": "/planechase/static/media/qm_gold.ed942dbd.svg"
+  },
+  {
+    "revision": "f90c67e5ad91d6531dd9c0956a47745d",
+    "url": "/planechase/static/media/quiksilver_sea.f90c67e5.jpg"
+  },
+  {
+    "revision": "a9ed7011eabba6cff6cdbaa2d0da820d",
+    "url": "/planechase/static/media/ravens_run.a9ed7011.jpg"
+  },
+  {
+    "revision": "c7001617171680a0e5423bd85eef7edb",
+    "url": "/planechase/static/media/reality_shaping.c7001617.jpg"
+  },
+  {
+    "revision": "9f4dad62dd1363d4685bb3bcad77db07",
+    "url": "/planechase/static/media/sanctum_of_serra.9f4dad62.jpg"
+  },
+  {
+    "revision": "d6195106063647b158015055fcdaf646",
+    "url": "/planechase/static/media/sea_of_sand.d6195106.jpg"
+  },
+  {
+    "revision": "2b1b32f649e39f9df8fd0773a85ecbaf",
+    "url": "/planechase/static/media/selesnya_loft_garden.2b1b32f6.jpg"
+  },
+  {
+    "revision": "f75e536881f8d8dd2adaf190dc564ee7",
+    "url": "/planechase/static/media/shiv.f75e5368.jpg"
+  },
+  {
+    "revision": "121abb8af97b005c70c52be0e9dc31c3",
+    "url": "/planechase/static/media/skybreen.121abb8a.jpg"
+  },
+  {
+    "revision": "10ddfb0a6f1a37896bfcc059b87247e4",
+    "url": "/planechase/static/media/sokenzan.10ddfb0a.jpg"
+  },
+  {
+    "revision": "2dc9323444643707359ecd68575a0629",
+    "url": "/planechase/static/media/spatial_merging.2dc93234.jpg"
+  },
+  {
+    "revision": "d7ce35d564f851c6d6babdbfc2617159",
+    "url": "/planechase/static/media/stairs_to_infinity.d7ce35d5.jpg"
+  },
+  {
+    "revision": "a342d1ed8d74f9be7a857cffef67d360",
+    "url": "/planechase/static/media/stensia.a342d1ed.jpg"
+  },
+  {
+    "revision": "bf88a54adcbcde79fc68009331beee4e",
+    "url": "/planechase/static/media/stronghold_furnace.bf88a54a.jpg"
+  },
+  {
+    "revision": "46b90e70abbe167a407b270483fed956",
+    "url": "/planechase/static/media/switch_current.46b90e70.svg"
+  },
+  {
+    "revision": "5ddbce81cc4583d43bf1827333a6303d",
+    "url": "/planechase/static/media/switch_overhead.5ddbce81.svg"
+  },
+  {
+    "revision": "3d61e451f03b6518ea7490e23a6db060",
+    "url": "/planechase/static/media/takenuma.3d61e451.jpg"
+  },
+  {
+    "revision": "6283a26183ee2cba05b11f82a9feeffc",
+    "url": "/planechase/static/media/tazeem.6283a261.jpg"
+  },
+  {
+    "revision": "957d991b9d006efba6df752c48d84b49",
+    "url": "/planechase/static/media/the_aether_flues.957d991b.jpg"
+  },
+  {
+    "revision": "ff39a3529862db07f1fe986b7b461f60",
+    "url": "/planechase/static/media/the_dark_barony.ff39a352.jpg"
+  },
+  {
+    "revision": "5e54131f5e4a0d5ba198f5a530cf671b",
+    "url": "/planechase/static/media/the_eon_fog.5e54131f.jpg"
+  },
+  {
+    "revision": "3aaa634bc66033a72f3f70a64e51b695",
+    "url": "/planechase/static/media/the_fourth_sphere.3aaa634b.jpg"
+  },
+  {
+    "revision": "9f10f502ffb4976a0814f7b184d7bef1",
+    "url": "/planechase/static/media/the_great_forest.9f10f502.jpg"
+  },
+  {
+    "revision": "9f2df314626d0402d23b029f3dd3e5d1",
+    "url": "/planechase/static/media/the_hippodrome.9f2df314.jpg"
+  },
+  {
+    "revision": "24a3552e00562f2b87387e5d824608d2",
+    "url": "/planechase/static/media/the_maelstrom.24a3552e.jpg"
+  },
+  {
+    "revision": "15eb0c075d695986fa77ac2803ae6c06",
+    "url": "/planechase/static/media/the_zephyr_maze.15eb0c07.jpg"
+  },
+  {
+    "revision": "d7dc88de272406f97d08c0b902b31984",
+    "url": "/planechase/static/media/time_distortion.d7dc88de.jpg"
+  },
+  {
+    "revision": "52a1f174e31bf814432d1524d6b5173b",
+    "url": "/planechase/static/media/trail_of_the_mage_rings.52a1f174.jpg"
+  },
+  {
+    "revision": "82de516ec0e2622dd345a60ef7c41e7e",
+    "url": "/planechase/static/media/truga_jungle.82de516e.jpg"
+  },
+  {
+    "revision": "4d29d48877ed8c4edf9fc9150eea5758",
+    "url": "/planechase/static/media/turri_island.4d29d488.jpg"
+  },
+  {
+    "revision": "37770c8359944cc762484f601d4da8ce",
+    "url": "/planechase/static/media/undercity_reaches.37770c83.jpg"
+  },
+  {
+    "revision": "eb70108c8d5f9da428de4a5fbf36c49c",
+    "url": "/planechase/static/media/velis_vel.eb70108c.jpg"
+  },
+  {
+    "revision": "6137d9509e295aac5a25f5abd8c04aea",
+    "url": "/planechase/static/media/windriddle_palaces.6137d950.png"
+  }
+]);
